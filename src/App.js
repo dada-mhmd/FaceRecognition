8324af6f4695cb8b1cc928eb2c40b27258fa2df5
dada-mhmd <moudi.ld@hotmail.com>
2021-09-18 12:07:17 +0300

    added rank components and background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
-import ImageLinkForm from "./components/imageLinkForm/LinkImageForm";
+import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
+import Rank from "./components/rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition />   */}
       </div>
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./imageLinkForm.css";
 
 const ImageLinkForm = () => {
   return (
@@ -6,6 +7,14 @@ const ImageLinkForm = () => {
       <p className="f3">
         {"This Magic Brain Will Detect faces in your pictures. Get it a try!"}
       </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="text" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
